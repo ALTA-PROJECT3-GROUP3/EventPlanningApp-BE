f6bde518d54ff1598c9423a5403f4f930e12a051
Kristain Putra <kputrapar@gmail.com>
2023-04-16 13:05:07 +0700

    build(github actions workflow, try to connect to server): Dockerfile and config are initialized
    on discord we can find the secrets to cross check each other

---
+++ app/database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/app/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDBMySql(cfg config.AppConfig) *gorm.DB {
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		cfg.DB_USER,
+		cfg.DB_PASS,
+		cfg.DB_HOST,
+		cfg.DB_PORT,
+		cfg.DB_NAME,
+	)
+
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		log.Println("error connect to database:", err.Error())
+		return nil
+	}
+	return db
+}
