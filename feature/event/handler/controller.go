dd2fddb63ac548715717d4db42ffacc5b84fad03
unknown <adiyudapranata@gmail.com>
2023-04-26 00:11:58 +0700

    feat: update add event controller

--- feature/event/handler/controller.go
+++ feature/event/handler/controller.go
@@ -22,22 +22,24 @@ func New(us event.UseCase) event.Handler {
 
 // AddHandler implements event.Handler
 func (ev *eventController) AddHandler() echo.HandlerFunc {
-	addInput := EventRequest{}
-	addInput.UserID = uint(helper.DecodeToken(c))
-	if err := c.Bind(&addInput); err != nil {
-		c.Logger().Error("terjadi kesalahan bind", err.Error())
-		return c.JSON(helper.ResponseFormat(http.StatusBadRequest, "invalid input", nil))
+	return func(c echo.Context) error {
+		addInput := EventRequest{}
+		addInput.UserID = uint(helper.DecodeToken(c))
+		if err := c.Bind(&addInput); err != nil {
+			c.Logger().Error("terjadi kesalahan bind", err.Error())
+			return c.JSON(helper.ResponseFormat(http.StatusBadRequest, "invalid input", nil))
+		}
+
+		newEvent := event.Core{}
+		copier.Copy(&newEvent, &addInput)
+
+		file, _ := c.FormFile("pictures")
+
+		err := ev.service.Add(newEvent, file)
+		if err != nil {
+			c.Logger().Error("terjadi kesalahan saat add Event", err.Error())
+			return c.JSON(helper.ResponseFormat(http.StatusInternalServerError, err.Error(), nil))
+		}
+		return c.JSON(helper.ResponseFormat(http.StatusCreated, "add event successfully", nil))
 	}
-
-	newEvent := event.Core{}
-	copier.Copy(&newEvent, &addInput)
-
-	file, _ := c.FormFile("pictures")
-
-	err := ev.service.Add(newEvent, file)
-	if err != nil {
-		c.Logger().Error("terjadi kesalahan saat add Event", err.Error())
-		return c.JSON(helper.ResponseFormat(http.StatusInternalServerError, err.Error(), nil))
-	}
-	return c.JSON(helper.ResponseFormat(http.StatusCreated, "add event successfully", nil))
 }
