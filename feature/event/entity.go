abbe3240a93e96398033d246bcc4719c097adf6a
unknown <adiyudapranata@gmail.com>
2023-04-25 22:45:41 +0700

    chore: create event core and add function interface

--- feature/event/entity.go
+++ feature/event/entity.go
@@ -1,1 +1,32 @@
 package event
+
+import (
+	"mime/multipart"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	Id          uint
+	Name        string
+	HostName    string
+	Description string
+	Date        time.Time
+	Location    string
+	IsPaid      bool
+	Pictures    string
+	UserID      uint
+}
+
+type Handler interface {
+	AddHandler() echo.HandlerFunc
+}
+
+type UseCase interface {
+	Add(newEvent Core, file *multipart.FileHeader) error
+}
+
+type Repository interface {
+	Insert(input Core) error
+}
