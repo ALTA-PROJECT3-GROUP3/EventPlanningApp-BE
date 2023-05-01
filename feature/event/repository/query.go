6935ecd47ff98f161fc06c685e8b85a41f141a51
unknown <adiyudapranata@gmail.com>
2023-05-01 14:46:20 +0700

    chore: create select all event repository

--- feature/event/repository/query.go
+++ feature/event/repository/query.go
@@ -16,6 +16,19 @@ func New(db *gorm.DB) event.Repository {
 	}
 }
 
+// SelectAll implements event.Repository
+func (ev *eventQuery) SelectAll(limit int, offset int, name string) ([]event.Core, error) {
+	nameSearch := "%" + name + "%"
+	var eventsModel []Event
+	tx := ev.db.Limit(limit).Offset(offset).Where("events.name LIKE ?", nameSearch).Select("events.id, events.name, events.host_name, events.description, events.date, events.location, events.is_paid, events.pictures").Joins("JOIN users ON events.user_id = users.id").Group("events.id").Find(&eventsModel)
+	if tx.Error != nil {
+		log.Error("Terjadi error saat select Book")
+		return nil, tx.Error
+	}
+	eventsCoreAll := ListModelToCore(eventsModel)
+	return eventsCoreAll, nil
+}
+
 // Insert implements event.Repository
 func (ev *eventQuery) Insert(input event.Core) error {
 	data := CoreToEvent(input)
