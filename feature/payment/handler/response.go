aec25a739747f25d1ce19221ca44571e083390c3
Kristain Putra <kputrapar@gmail.com>
2023-05-01 22:21:48 +0700

    feat(reservation): Only bank transfer BCA
    all midtrans object response temporary saved on db

--- feature/payment/handler/response.go
+++ feature/payment/handler/response.go
@@ -1,1 +1,5 @@
 package handler
+
+type PaymentResponse struct {
+    PaymentID uint `json:"payment_id"`
+}
--- feature/payment/repository/query.go
+++ feature/payment/repository/query.go
@@ -1,1 +1,111 @@
 package repository
+
+import (
+	"errors"
+
+	"github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/payment"
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type paymentModel struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) payment.Repository {
+	return &paymentModel{
+		db: db,
+	}
+}
+
+func (pm *paymentModel) CheckEvent(rsv payment.ReservationsCore) error {
+	var count int64
+	if err := pm.db.Raw("SELECT id FROM events WHERE id = ?", rsv.EventID).Count(&count).Error; err != nil {
+		log.Error("error occured when finding event for reservation")
+		return err
+	}
+
+	if count == 0 {
+		log.Error("count is 0, event not found")
+		return errors.New("event not found")
+	}
+
+	return nil
+}
+
+func (pm *paymentModel) CheckTicket(rsv payment.ReservationsCore) (payment.ReservationsCore, error) {
+	var (
+		count    int64
+		ticketDB struct {
+			TicketID uint
+			Name     string
+			Quota    int
+			Price    int
+			EventID  uint
+		}
+	)
+
+	type addTicket struct {
+		TicketID uint
+		Name     string
+		Quantity int
+		Quota    int
+		Price    int
+	}
+
+	for _, ticket := range rsv.Tickets {
+		if err := pm.db.Raw("SELECT id, name, quota, price, event_id FROM tickets WHERE id = ? AND event_id = ?", ticket.TicketID, rsv.EventID).Scan(&ticketDB).Count(&count).Error; err != nil {
+			log.Error("error occurred in finding tickets for reservations")
+			return rsv, err
+		}
+
+		rsv.Tickets = append(rsv.Tickets, addTicket{
+			TicketID: ticketDB.TicketID,
+			Name:     ticketDB.Name,
+			Quantity: ticket.Quantity,
+			Quota:    ticketDB.Quota,
+			Price:    ticketDB.Price,
+		})
+	}
+
+	return rsv, nil
+}
+
+func (pm *paymentModel) InsertPayment(paymentRsv payment.PaymentCore) error {
+	var (
+		paymentDB Payment
+	)
+
+	paymentDB.PaymentType = paymentRsv.PaymentType
+	paymentDB.VA = paymentRsv.VA
+	paymentDB.Bank = paymentRsv.Bank
+	paymentDB.Status = paymentRsv.Status
+	paymentDB.GrandTotal = paymentRsv.GrandTotal
+	paymentDB.UserID = paymentRsv.UserID
+	paymentDB.OrderID = paymentRsv.OrderID
+
+	if err := pm.db.Create(&paymentDB).Error; err != nil {
+		log.Error("error occured in inserting payment")
+		return err
+	}
+	return nil
+
+}
+
+func (pm *paymentModel) InsertPaymentDetails(rsv payment.PaymentDetailCore) error {
+	var (
+		paymentDB PaymentDetail
+	)
+
+	paymentDB.TicketID = rsv.TicketID
+	paymentDB.Total = rsv.Total
+	paymentDB.PaymentID = rsv.PaymentID
+	paymentDB.Qty = rsv.Qty
+
+	if err := pm.db.Create(&paymentDB).Error; err != nil {
+		log.Error("error occured in inserting payment detail")
+		return err
+	}
+	return nil
+
+}
