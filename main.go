a2124ae7a90d2606ee30de1e735e07b4ecd335b2
unknown <adiyudapranata@gmail.com>
2023-05-03 00:22:27 +0700

    create event routes on main

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	commentHandler "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/comment/handler"
 	commentRepo "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/comment/repository"
 	commentLogic "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/comment/usecase"
+	eHandler "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/event/handler"
+	eRepo "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/event/repository"
+	eLogic "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/event/usecase"
 	uHandler "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/user/handler"
 	uRepo "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/user/repository"
 	uLogic "github.com/ALTA-PROJECT3-GROUP3/EventPlanningApp-BE/feature/user/usecase"
@@ -27,8 +30,13 @@ func main() {
 	commentSrv := commentLogic.New(commentMdl)
 	commentCtl := commentHandler.New(commentSrv)
 
+	eMdl := eRepo.New(db)
+	eSrv := eLogic.New(eMdl)
+	eCtl := eHandler.New(eSrv)
+
 	routes.UserRoutes(e, uCtl)
 	routes.CommentRoutes(e, commentCtl)
+	routes.EventRoutes(e, eCtl)
 
 	if err := e.Start(":8080"); err != nil {
 		e.Logger.Fatal("cannot start server", err.Error())
